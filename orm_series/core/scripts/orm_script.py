5afebd10af3586517ee9a4f268c7ca9dffa33839
Mc Clareenz Zerrudo <mc.zerrudo@ssed-academy.opswerks.com>
2025-08-07 15:29:22 +0800

    5th Video Done

--- orm_series/core/scripts/orm_script.py
+++ orm_series/core/scripts/orm_script.py
@@ -6,13 +6,11 @@ from pprint import pprint
 def run():
     User = get_user_model()
     # Create a new restaurant
-    restaurant = Restaurant.objects.first()
-    user = User.objects.first()
-    print(restaurant.name)
-    
-    restaurant.name = 'A New Restaurant Name'
-    restaurant.save(update_fields=['name'])  # Save the changes to the database
-   
+    restaurants = Restaurant.objects.filter(name__startswith='A')
+    print(restaurants)
+    print(restaurants.update(
+        date_opened = timezone.now() -timezone.timedelta(days=365),
+    ))
     pprint(connection.queries)  # Print all SQL queries executed during this run
 
 
